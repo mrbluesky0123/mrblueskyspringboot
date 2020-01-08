91fe325eba558cf7e4549a6b4ac638bbf6c1ba97
강승보(KANG SEUNGBO)/통신/미디어Digital추진그룹/SK <09626@skcc.com>
2020-01-08 17:05:22 +0900

    First commit

---
+++ src/main/java/me/mrbluesky/mrblueskyspringbootstarter/MrblueskyspringbootstarterApplication.java
@@ -0,0 +1,13 @@
+package me.mrbluesky.mrblueskyspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrblueskyspringbootstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrblueskyspringbootstarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mrbluesky/mrblueskyspringbootstarter/MrblueskyspringbootstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mrbluesky.mrblueskyspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrblueskyspringbootstarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
